2adb77e120b4ae5466fe1418aaf19c44cf11215d
eligarc <elioenaig041294@gmail.com>
2024-03-18 21:08:45 -0600

    :sparkles: Se agregan relaciones uno a muchos - hasMany & belongsTo en orders/customer

--- services/order.service.js
+++ services/order.service.js
@@ -1,19 +1,28 @@
 const boom = require('@hapi/boom');
+const { models } = require('../libs/sequelize');
 
 class OrderService {
 
-  constructor(){
+  constructor() {
   }
   async create(data) {
-    return data;
+    const newOrder = await models.Order.create(data);
+    return newOrder;
   }
 
   async find() {
-    return [];
+    const orders = await models.Order.findAll();
+    return orders;
   }
 
   async findOne(id) {
-    return { id };
+    const order = await models.Order.findByPk(id, {
+      include: [{
+        association: 'customer',
+        include: ['user']
+      }]
+    });
+    return order;
   }
 
   async update(id, changes) {
